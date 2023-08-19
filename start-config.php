9b90ff1942907cb725bb39da8553b39539b55c28
Johan Rojas <97570183+redsnahoj@users.noreply.github.com>
2023-08-18 20:17:33 -0400

    Add validator to database connection form

--- start-config.php
+++ start-config.php
@@ -6,24 +6,32 @@ if ($_POST) {
     $dbPasswordPost = $_POST["password"];
     $dbserverPost = $_POST["dbserver"];
 
-    $codeSetting = "<?php
+    try {
+        $pdo = new PDO("mysql:host=$dbserverPost;dbname=$dbnamePost", $usernamePost, $dbPasswordPost);
+        echo "¡Conectado correctamente!";
+        $codeSetting = "<?php
 
-    \$dbName = '$dbnamePost';
-    \$username = '$usernamePost';
-    \$dbPassword = '$passwordPost';
-    \$dbServer = '$dbserverPost';
+        \$dbName = '$dbnamePost';
+        \$username = '$usernamePost';
+        \$dbPassword = '$passwordPost';
+        \$dbServer = '$dbserverPost';
 
-    ?>";
+        ?>";
 
-    $settingDoc = fopen("setting.php", 'w') or 
-    die("Se produjo un error al crear el archivo");
+        $settingDoc = fopen("setting.php", 'w') or 
+        die("Se produjo un error al crear el archivo");
 
-    fwrite($settingDoc, $codeSetting) or die("No se pudo escribir en el archivo");
-  
-    fclose($settingDoc);
+        fwrite($settingDoc, $codeSetting) or die("No se pudo escribir en el archivo");
+    
+        fclose($settingDoc);
+    } catch (PDOException $e) {
+        echo "Error al conectar a la base de datos: " . $e->getMessage();
+    }
+
 }
 ?>
 
+<?php if(!is_file('setting.php')) {?>
 <form action="" method="post">
     <label for="dbname">Nombre de la base de datos</label><br>
     <input type="text" id="dbname" name="dbname"><br>
@@ -36,3 +44,4 @@ if ($_POST) {
     <br>
     <button type="submit">Enviar</button>
 </form>
+<?php } ?>
